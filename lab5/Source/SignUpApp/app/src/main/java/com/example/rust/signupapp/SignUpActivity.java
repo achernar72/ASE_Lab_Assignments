a719784e1ed8d0e88445dbb2fd4e803eb6830a4d
aspk4 <sankarasettyavinash@gmail.com>
2017-02-22 16:34:00 -0600

    lab5 created

---
+++ lab5/Source/SignUpApp/app/src/main/java/com/example/rust/signupapp/SignUpActivity.java
@@ -0,0 +1,57 @@
+package com.example.rust.signupapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignUpActivity extends AppCompatActivity {
+    private static EditText username;
+    private static EditText password;
+    private static Button Login;
+    private static Button signup;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        loginButton();
+    }
+    public void loginButton(){
+        username =(EditText)findViewById(R.id.etUsername);
+        password =(EditText)findViewById(R.id.etPassword);
+        Login =(Button)findViewById(R.id.btLogin);
+        signup =(Button)findViewById(R.id.btSignIn);
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(username.getText().toString().equals("aspk4")&&
+                        password.getText().toString().equals("aspk4")) {
+                    Intent intent = new Intent("com.example.rust.signupapp.HomePage");
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(SignUpActivity.this,"user and password is not correct",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v1) {
+                Intent launchActivity1= new Intent("com.example.rust.signupapp.registerActivity");
+                startActivity(launchActivity1);
+
+            }
+        });
+
+
+
+    }
+
+
+}
